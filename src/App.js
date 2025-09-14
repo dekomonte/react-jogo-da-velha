b80e44bff3bbbe6b68940cac5cbad0df6e01e5c6
Andressa Monteiro <dekomonte@yahoo.com.br>
2025-09-14 10:50:17 -0300

    feat: x ao clicar

--- src/App.js
+++ src/App.js
@@ -35,9 +35,9 @@ export default function Board() {
 
   const[squares, setSquares] = useState(Array(9).fill(null));
 
-  function handleClick(){
+  function handleClick(i){
     const nextSquares = squares.slice();
-    nextSquares[0] = "x";
+    nextSquares[i] = "x";
     setSquares(nextSquares);
   }
 
@@ -45,21 +45,21 @@ export default function Board() {
     <>
 
       <div className="linha-tabuleiro">
-        <Square value={squares[0]} onSquareClick={handleClick}/>
-        <Square value={squares[1]} onSquareClick={handleClick}/>
-        <Square value={squares[2]} onSquareClick={handleClick}/>
+        <Square valor={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square valor={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square valor={squares[2]} onSquareClick={() => handleClick(2)} />
       </div>
 
       <div className="linha-tabuleiro">
-        <Square value={squares[3]} onSquareClick={handleClick}/>
-        <Square value={squares[4]} onSquareClick={handleClick}/>
-        <Square value={squares[5]} onSquareClick={handleClick}/>
+        <Square valor={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square valor={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square valor={squares[5]} onSquareClick={() => handleClick(5)} />
       </div>
 
       <div className="linha-tabuleiro">
-        <Square value={squares[6]} onSquareClick={handleClick}/>
-        <Square value={squares[7]} onSquareClick={handleClick}/>
-        <Square value={squares[8]} onSquareClick={handleClick}/>
+        <Square valor={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square valor={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square valor={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
 
     </>
